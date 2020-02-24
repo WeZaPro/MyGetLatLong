259fff3aea0e77cf95ccdc88d3afb8deb5dbf279
WeZaPro <taweesak9359@gmail.com>
2020-02-24 14:50:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mygetlatlong/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.mygetlatlong;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvLat,tvLon;
+    Button btnClick;
+    FusedLocationProviderClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvLat = findViewById(R.id.tvLat);
+        tvLon = findViewById(R.id.tvLon);
+        btnClick = findViewById(R.id.btnClick);
+
+        //get Permission
+        requestPermissions();
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocationAddress();
+            }
+        });
+
+    }
+
+    private void getLocationAddress() {
+        client.getLastLocation().addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+
+                if (location != null) {
+                    tvLat.setText(""+location.getLatitude());
+                    tvLon.setText(""+location.getLongitude());
+                }
+            }
+        });
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+    }
+}
